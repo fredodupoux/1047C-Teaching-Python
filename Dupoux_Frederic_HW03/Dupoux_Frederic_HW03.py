f35f6b61aa62cc8c3af842202feaf6a9bce16a31
Frederic <fredodupoux@gmail.com>
2025-02-24 13:02:28 -0500

    cleaned up and fixed bug

--- Dupoux_Frederic_HW03/Dupoux_Frederic_HW03.py
+++ Dupoux_Frederic_HW03/Dupoux_Frederic_HW03.py
@@ -30,151 +30,106 @@ totalAmountDue: float = 0.0
 
 # Print welcome message to user
 print("\n\n*********** Welcome to the Picture Framing Shop! ***********\n")
+# Prime values to get in the loop
+numberOfFrames: int = 1
 
-# prime value to get in the loop
-validNumberOfFrames: bool = True
-
-# WHILE Number of Frames is NOT valid
-while validNumberOfFrames:
+#WHILE Number of Frames > 0
+while numberOfFrames > 0:
     #TRY
     try:
-        #PROMPT: Input Number Of Frames: Sentinel value <= 0
-        numberOfFrames: int = int(input("Enter the number of frames being purchased or -1 to quit: "))
-        #IF - Check for Sentinel value
-        if numberOfFrames <= 0:
-            validNumberOfFrames = False # exit the loop and quit
-        #ELSE
-        else:
-            #IF - Validate Range    
-            if numberOfFrames not in range(MIN_FRAME, MAX_FRAME + 1):
-                print(f"Please enter a number in the range of {MIN_FRAME} to {MAX_FRAME} frames: ")
-            #ELSE    
-            else:
-                #Determine Discount Rate based on Number of Frames
-                #IF
+        #PROMPT: Input number of frames
+        numberOfFrames = int(
+            input("Enter -1 to end sales or the number of frames being purchased: "))
+        #IF - Validate Sentinel values
+        if numberOfFrames > 0:
+            # Set default values for discountRate & validFramePrice
+            discountRate : float = 0.00 
+            validFramePrice: bool = False 
+            
+            #IF - Validate range of frames
+            if numberOfFrames not in range(MIN_FRAME, MAX_FRAME+1):
+                print(f"Please enter a number in the range of {MIN_FRAME} to {MAX_FRAME} frames.")
+            #ELSE - Determine Discount Rate based on Number of Frames
+            else: 
+                # IF
                 if 1 <= numberOfFrames <= 5:
                     discountRate : float = 0.00
                 elif 6 <= numberOfFrames <= 10:
                     discountRate : float = 0.05
                 elif 11 <= numberOfFrames <= 15:
                     discountRate : float = 0.10
-                #ELSE
+                # ELSE
                 else :
                     discountRate : float = 0.15
-                #Exit the loop.
-                validNumberOfFrames = False
+                # Prime Value to get in next Loop
+                validFramePrice: bool = True
             #ENDIF
-        #ENDIF  
-    #EXCEPTION
-    except:
-        #Stay in the loop and print non numeric error message
-        print("Please input only numeric values.")
-#END WHILE
 
-#WHILE Number of Frames > 0
-while numberOfFrames > 0:
-    #TRY
-    try:
-        # Set value to get in the loop
-        validFramePrice: bool = True
-        #WHILE frame price is NOT valid
-        while validFramePrice:
-            #TRY
-            try:
-                #PROMPT: Input Price of Frame
-                framePrice: float = float(input("Enter the price for the frames: "))
-                #IF - Validate frame price range
-                if framePrice < PRICE_RANGE_MIN or framePrice > PRICE_RANGE_MAX:
-                    print(f"Enter a valid price ranging from {PRICE_RANGE_MIN} to {PRICE_RANGE_MAX}")
-                #ELSE
-                else :
-                    #Calculate Cost Of Frames (Number of Frames * Price of Frames)
-                    costOfFrames: float = framePrice * numberOfFrames
-                    # Calculate discount amount
-                    discountAmount : float = costOfFrames * discountRate
-                    # Calculate net amount
-                    netAmount : float = costOfFrames - discountAmount
-                    # Calculate Sales Tax
-                    salesTax : float = netAmount * SALES_TAX_RATE
-                    # Calculate Amount Due
-                    amountDue : float = netAmount + salesTax
-                    # Print and Display formatted output
-                    print("\n")
-                    print(f"Order details for {numberOfFrames} frames priced at ${framePrice}\n")
-                    print(f"Cost of frames:             ${costOfFrames:>10,.2f}")
-                    # Display discount and net amount only if discount is applied
-                    if numberOfFrames > 5:
-                        print(f"Discount: {int(discountRate*100):<2}%               ${discountAmount:>10,.2f}")
-                        print(f"Net Amount:                 ${netAmount:>10,.2f}")
-                    # Display Sales Tax and Total Amount Due
-                    print(f"Sales Tax:                  ${salesTax:>10,.2f}")
-                    print("----------------------------------------")
-                    print(f"Total due:                  ${amountDue:>10,.2f}")
-                    print("\n")
-                    # Exit Frame Price loop
-                    validFramePrice = False
-                #ENDIF
-            #Catch Exceptions:
-            except:
-                #Stay in the price loop and print non numeric error message
-                print("Please input only numeric values.")
-        #ENDWHILE 
+            #WHILE frame price is valid
+            while validFramePrice:
+                #TRY
+                try:
+                    #PROMPT: Input Price of Frame
+                    framePrice: float = float(input("Enter the price for the frames: "))
+                    #IF - Validate frame price range
+                    if framePrice < PRICE_RANGE_MIN or framePrice > PRICE_RANGE_MAX:
+                        print(f"Enter a valid price ranging from {PRICE_RANGE_MIN} to {PRICE_RANGE_MAX}")
+                    #ELSE
+                    else :
+                        #Calculate Cost Of Frames (Number of Frames * Price of Frames)
+                        costOfFrames: float = framePrice * numberOfFrames
+                        # Calculate discount amount
+                        discountAmount : float = costOfFrames * discountRate
+                        # Calculate net amount
+                        netAmount : float = costOfFrames - discountAmount
+                        # Calculate Sales Tax
+                        salesTax : float = netAmount * SALES_TAX_RATE
+                        # Calculate Amount Due
+                        amountDue : float = netAmount + salesTax
+                        # Print and Display formatted output
+                        print("\n")
+                        print(f"Order details for {numberOfFrames} frames priced at ${framePrice}\n")
+                        print(f"Cost of frames:             ${costOfFrames:>10,.2f}")
+                        # Display discount and net amount only if discount is applied
+                        if numberOfFrames > 5:
+                            print(f"Discount: {int(discountRate*100):<2}%               ${discountAmount:>10,.2f}")
+                            print(f"Net Amount:                 ${netAmount:>10,.2f}")
+                        # Display Sales Tax and Total Amount Due
+                        print(f"Sales Tax:                  ${salesTax:>10,.2f}")
+                        print("----------------------------------------")
+                        print(f"Total due:                  ${amountDue:>10,.2f}")
+                        print("\n")
 
-        #Accumulte Totals
-        totalNumberOfCustomer += 1
-        totalNumberOfFrames += numberOfFrames
-        totalCostOfFrames += costOfFrames
-        totalDiscount += discountAmount
-        totalSalesTax += salesTax
-        totalAmountDue += amountDue
-
-        # prime value to get in the loop
-        validNumberOfFrames = True
-        # WHILE: number of frames is not valid
-        while validNumberOfFrames:
-            #TRY
-            try:
-                #PROMPT: Input Number Of Frames any number less then zero closes the program
-                numberOfFrames = int(input("Enter -1 to end sales or the number of frames being purchased: "))
-                #IF - check Sentinel value
-                if numberOfFrames <= 0:
-                    # exit loop
-                    validNumberOfFrames = False
-                #ELSE 
-                else:
-                    #IF - Validate range of frames
-                    if numberOfFrames in range(MIN_FRAME, MAX_FRAME+1):
-                        #reset valid number of frames to exit loop
-                        validNumberOfFrames = False
-                        #Determine Discount Rate based on Number of Frames
-                        # IF
-                        if 1 <= numberOfFrames <= 5:
-                            discountRate : float = 0.00
-                        elif 6 <= numberOfFrames <= 10:
-                            discountRate : float = 0.05
-                        elif 11 <= numberOfFrames <= 15:
-                            discountRate : float = 0.10
-                        # ELSE
-                        else :
-                            discountRate : float = 0.15
-                        #ENDIF
-                    #ELSE - Input valid range.
-                    else: 
-                        print(f"Enter a number between {MIN_FRAME} and {MAX_FRAME} frames.")
+                        #Accumulte Totals
+                        totalNumberOfCustomer += 1
+                        totalNumberOfFrames += numberOfFrames
+                        totalCostOfFrames += costOfFrames
+                        totalDiscount += discountAmount
+                        totalSalesTax += salesTax
+                        totalAmountDue += amountDue
+                        # Exit current loop
+                        validFramePrice = False
                     #ENDIF
-            #Catch Exception
-            except:
-                #Print non numeric error message
-                print("Please input only numeric values.")
-        #END WHILE
+                #Catch Exceptions:
+                except:
+                    #Stay in the price loop and print non numeric error message
+                    print("Please input only numeric values for the price of frames.")       
+            #ENDWHILE 
+        #ENDIF
     #Catch Exception
     except:
         #Print non numeric error message
-        print("Please input only numeric values.")
+        print("Please input only numeric values for the number of frames.")
 #END WHILE
 
 #IF customer Close Sales
-if totalNumberOfCustomer > 0:
+if totalNumberOfCustomer <= 0:
+    print("""
+      *********** BYE ***********
+
+      """)
+#ELSE quit and no print
+else: 
     print("""
       *********** CLOSING SALES ***********
                     *********
@@ -196,10 +151,4 @@ if totalNumberOfCustomer > 0:
     print("--------------------------------------------")
     print(f"GRAND TOTAL DUE:                ${totalAmountDue:>10,.2f}")
     print("\n")
-#ELSE quit and no print
-else: 
-    print("""
-      *********** BYE ***********
-
-      """)
 #ENDIF
